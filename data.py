1d146702341467441e293e3c6b8938f76041a1b8
= <vicjuma945@gmail.com>
2020-02-10 05:34:00 +0300

    monthly summary totals function implementation

---
+++ data.py
@@ -0,0 +1,56 @@
+from flask import Flask
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///invoice.db'
+db = SQLAlchemy(app)
+
+# function to find the monthly totals summary QUIZ1
+def monthly_totals():
+    jan2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Jan2019 FROM invoices WHERE [*invoicedate] LIKE "%/1/2019"', con=db.engine).to_dict()["Jan2019"][0]
+
+    feb2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Feb2019 FROM invoices WHERE [*invoicedate] LIKE "%/2/2019"', con=db.engine).to_dict()["Feb2019"][0]
+
+    mar2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Mar2019 FROM invoices WHERE [*invoicedate] LIKE "%/3/2019"', con=db.engine).to_dict()["Mar2019"][0]
+
+    apr2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Apr2019 FROM invoices WHERE [*invoicedate] LIKE "%/4/2019"', con=db.engine).to_dict()["Apr2019"][0]
+
+    may2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS May2019 FROM invoices WHERE [*invoicedate] LIKE "%/5/2019"', con=db.engine).to_dict()["May2019"][0]
+
+    jun2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Jun2019 FROM invoices WHERE [*invoicedate] LIKE "%/6/2019"', con=db.engine).to_dict()["Jun2019"][0]
+
+    jul2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Jul2019 FROM invoices WHERE [*invoicedate] LIKE "%/7/2019"', con=db.engine).to_dict()["Jul2019"][0]
+
+    aug2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Aug2019 FROM invoices WHERE [*invoicedate] LIKE "%/8/2019"', con=db.engine).to_dict()["Aug2019"][0]
+
+    sep2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Sep2019 FROM invoices WHERE [*invoicedate] LIKE "%/9/2019"', con=db.engine).to_dict()["Sep2019"][0]
+
+    oct2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Oct2019 FROM invoices WHERE [*invoicedate] LIKE "%/10/2019"', con=db.engine).to_dict()["Oct2019"][0]
+
+    nov2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Nov2019 FROM invoices WHERE [*invoicedate] LIKE "%/11/2019"', con=db.engine).to_dict()["Nov2019"][0]
+
+    dec2019 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Dec2019 FROM invoices WHERE [*invoicedate] LIKE "%/12/2019"', con=db.engine).to_dict()["Dec2019"][0]
+
+    jan2020 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Jan2020 FROM invoices WHERE [*invoicedate] LIKE "%/1/2020"', con=db.engine).to_dict()["Jan2020"][0]
+
+    feb2020 = pd.read_sql('SELECT SUM([*unitamount] * [*quantity]) AS Feb2020 FROM invoices WHERE [*invoicedate] LIKE "%/2/2020"', con=db.engine).to_dict()["Feb2020"][0]
+
+    monthly = {
+        "first": jan2019,
+        "second": feb2019,
+        "third": mar2019,
+        "forth": apr2019,
+        "fifth": may2019,
+        "sixth": jun2019,
+        "seventh": jul2019,
+        "eighth": aug2019,
+        "ninth": sep2019,
+        "tenth": oct2019,
+        "eleventh": nov2019,
+        "twelveth": dec2019,
+        "thirteenth": jan2020,
+        "forteenth": feb2020 
+    }
+    return monthly
