8595cb2bd541f77966b2fa8d47b3ee427c259a11
= <vicjuma945@gmail.com>
2020-02-10 04:06:22 +0300

    setting up DB and configuration variables

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # importing app instance dependency modules
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# initializing my app instance
+# initializing my app instance and setting up DB and configuration variables
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///invoice.db'
+app.secret_key = 'victoroluochjumafrommouseyincorporation'
+db = SQLAlchemy(app)
 
 
 
